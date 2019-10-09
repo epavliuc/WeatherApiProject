596cba29deafa609064c07431b3b385101227a5e
Eduard Pavliuc <epavliuc@spartaglobal.com>
2019-10-09 10:20:16 +0100

    Initial Commit/Established Work framework

---
+++ WeatherAPI/WeatherApi/HTTP_Management/WeatherApiCall.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using System.Configuration;
+
+namespace WeatherAPI.WeatherApi.HTTP_Management
+{
+    public class WeatherApiCall
+    {
+
+        public JObject WeatherApiResponse { get; set; }
+        public IRestClient Client { get; set; }
+        public IRestResponse Response { get; set; }
+
+        public static class ApiConfig
+        {
+            //settup limits to searching by city only
+            public static string ApiUrl = ConfigurationManager.AppSettings["BaseApiUrl"];
+            public static string ApiKey = ConfigurationManager.AppSettings["ApiKey"];
+            public static string ApiMod = ConfigurationManager.AppSettings["ApiMod"];
+        }
+        public void WeatherApiRequest(string city)
+        {
+            var request = new RestRequest();
+            Client = new RestClient(ApiConfig.ApiUrl + city + ApiConfig.ApiMod + ApiConfig.ApiKey);
+            Response = Client.Execute(request, Method.GET);
+        }
+    }
+}
---
+++ WeatherAPI/WeatherApiTests/WeatherApiTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WeatherAPI.WeatherApiTests
+{
+
+    [TestFixture]
+    public class WeatherApiTest
+    {
+        public WeatherApiTest()
+        {
+
+        }
+
+
+        [Test]
+        public void WeatherApiStatusCheck()
+        {
+
+        }
+    }
+}
